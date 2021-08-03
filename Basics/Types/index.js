ff66aedaa959d5fa78b73d7a80b7597577b96068
Muhammad-Salman-Hassan <85054404+Muhammad-Salman-Hassan@users.noreply.github.com>
2021-08-03 12:39:36 -0700

    Add files via upload

---
+++ Basics/Types/index.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var a = "Salman";
+var b = 10;
+var c = true;
+console.log(a, b, c);
---
+++ Basics/Types/index.ts
@@ -0,0 +1,6 @@
+export {}
+let a:string="Salman";
+let b:number=10;
+let c:boolean=true;
+
+console.log(a,b,c);
