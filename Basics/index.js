faee585a3f5f7a777aa4c4d60b8256ecd3d29131
Muhammad-Salman-Hassan <85054404+Muhammad-Salman-Hassan@users.noreply.github.com>
2021-08-03 12:13:57 -0700

    Add files via upload

---
+++ Basics/index.js
@@ -0,0 +1,10 @@
+var APP = /** @class */ (function () {
+    function APP() {
+    }
+    APP.prototype.test = function () {
+        console.log("Hello World");
+    };
+    return APP;
+}());
+var a1 = new APP();
+a1.test();
---
+++ Basics/index.ts
@@ -0,0 +1,8 @@
+class APP{
+    test()
+    {
+        console.log("Hello World")
+    }
+}
+let a1 = new APP();
+a1.test()
