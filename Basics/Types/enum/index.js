03801c67b59072fa717e79af11a9ee32e3933703
Muhammad-Salman-Hassan <85054404+Muhammad-Salman-Hassan@users.noreply.github.com>
2021-08-05 01:17:10 -0700

    Add files via upload

---
+++ Basics/Types/enum/index.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+var ColorRed = 0;
+var ColorGreen = 1;
+var ColorBlue = 2;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var BackgroundColor = Color.Green;
---
+++ Basics/Types/enum/index.ts
@@ -0,0 +1,9 @@
+export {}
+
+const ColorRed=0
+const ColorGreen=1
+const ColorBlue=2
+
+enum Color{Red=0,Green=1,Blue=2};
+
+let BackgroundColor=Color.Green;
